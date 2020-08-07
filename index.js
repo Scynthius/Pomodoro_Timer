812277f476ccfefed6b1f3e4266a6b3f59a727c1
Jonathan Fryman <frymanj@flip3.engr.oregonstate.edu>
2020-08-07 16:17:54 -0700

    Added route for new task and new category

--- index.js
+++ index.js
@@ -55,6 +55,34 @@ app.get('/', function(req,res){
   })
 });
 
+app.put('/', (req, res, next) => {
+  var useremail = req.user.email;
+  var user = users[0].find(user => user.email === useremail);
+  var userid = user.id;
+  var status = 200;
+  var queryString = "INSERT INTO tasks (name, task_time, break_time, userid, categoryid) VALUES ((?), (?), (?), (?), (?));";
+  if (req.body.newCategory){
+    var newCatQuery = "INSERT INTO categories (name, userid) VALUES ((?), (?));";
+    var getCatIDQuery = "SELECT MAX(id) FROM categories;";
+    postQuery(newCatQuery, [req.body.category, userid])
+    .then((result) => {
+      status = 200;
+    }).then(() => {
+      return getQuery(getCatIDQuery);
+    }).then((rows) => {
+      let category = rows[0].id;
+    }).then(() => {
+      postQuery(queryString, [req.body.name, req.body.taskTime, req.body.breakTime, userid, category])
+    }).then((result) => {
+      res.sendStatus(200);
+    })
+  } else {
+    postQuery(queryString, [req.body.name, req.body.taskTime, req.body.breakTime, userid, req.body.category])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+  }
+});
 
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/',
